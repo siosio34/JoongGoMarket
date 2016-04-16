06663bb0cc4b17f5e0c55833c31860ad37b367a6
young <siosio3103@gmail.com>
2016-04-16 08:59:57 -0700

    seller user add attribute

--- db/migrate/20160415053909_create_seller_users.rb
+++ db/migrate/20160415053909_create_seller_users.rb
@@ -1,6 +1,8 @@
 class CreateSellerUsers < ActiveRecord::Migration
   def change
     create_table :seller_users do |t|
+      t.integer :sellcount
+      t.integer :profit
 
       t.timestamps null: false
     end
